91a6374006fb9ba56b84b3c8b7e7eadc24b2094c
Zorca Orcinus <vs@zorca.org>
2020-01-31 15:59:39 +0300

    Constants for keys added

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+define('TODO_PREFIX', 'todo_');
+define('TODO_INDEX_KEY', TODO_PREFIX . 'index');
+define('TODO_ITEM_PREFIX', TODO_PREFIX . 'item_');
+
 require __DIR__ . '/vendor/autoload.php';
 
 new \App\Api();
